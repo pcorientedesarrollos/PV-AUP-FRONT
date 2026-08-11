ece80f8f9dea756108c8959b8c76119f9e6685ee
Emmanuel Manzanilla <emazanillabaas@gmail.com>
2026-08-11 11:02:32 -0600

    cambio de environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,6 @@
 // Cambiar apiUrl al dominio real del servidor antes de hacer el build de producción
 export const environment = {
   production: true,
-  apiUrl: 'http://TU-SERVIDOR-AQUI:3000', // ← CAMBIAR ESTE VALOR antes del deploy
+  // apiUrl: 'http://TU-SERVIDOR-AQUI:3000', // ← CAMBIAR ESTE VALOR antes del deploy
+  apiUrl: 'pv-aup-back-production.up.railway.app',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,5 +2,8 @@
 // Para producción, modificar apiUrl al dominio real del servidor
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:3000',
+  // apiUrl: 'http://localhost:3000',
+    apiUrl: 'pv-aup-back-production.up.railway.app',
+
 };
+
