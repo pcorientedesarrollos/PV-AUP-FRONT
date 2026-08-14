04c4b39f0f10bc0de366cd67e6c20df092106140
Emmanuel Manzanilla <emazanillabaas@gmail.com>
2026-08-14 12:11:47 -0600

    respaldo POS base 14-8-26

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -1,6 +1,6 @@
 export const environment = {
   name: 'dev',
   production: false,
-  // apiUrl: 'http://localhost:3000',
-  apiUrl: 'https://pv-aup-back-production.up.railway.app',
+  apiUrl: 'http://localhost:3000',
+  // apiUrl: 'https://pv-aup-back-production.up.railway.app',
 };
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
   name: 'prod',
   production: true,
-  apiUrl: 'https://pv-aup-back-production.up.railway.app',
+  apiUrl: 'http://localhost:3000',
+  // apiUrl: 'https://pv-aup-back-production.up.railway.app',
 };
