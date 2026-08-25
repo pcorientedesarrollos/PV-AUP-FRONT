4eb9ff8a640fe41f2e1599f6a54bf97204032d39
Emmanuel Manzanilla <emazanillabaas@gmail.com>
2026-08-25 13:24:49 -0600

    eliminacion de js

--- fix_encoding2.js
+++
@@ -1,5 +0,0 @@
-const fs = require('fs');
-const file = 'src/app/features/produccion/produccion.component.ts';
-let content = fs.readFileSync(file, 'latin1'); // Read as latin1 to preserve weird bytes
-content = content.replace(/Comod\ufffdn|Comod\xeen|Comod.n/g, 'Comodín');
-fs.writeFileSync(file, content, 'utf8'); // Save as UTF-8!
--- fix_prod.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-const file = 'src/app/features/produccion/produccion.component.ts';
-let content = fs.readFileSync(file, 'utf8');
-
-content = content.replace(/Comod\ufffdn/g, 'Comodín');
-content = content.replace("next: (data) => { \r\n        this.productos.set(data || []);", "next: (res: any) => { \r\n        const data = res.data || res || []; \r\n        this.productos.set(data);");
-content = content.replace("next: (data) => { \n        this.productos.set(data || []);", "next: (res: any) => { \n        const data = res.data || res || []; \n        this.productos.set(data);");
-
-fs.writeFileSync(file, content, 'utf8');
